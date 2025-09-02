116229c6204477a9836f5aeefe2261b55e9b93f0
Prudhvigurubilli04 <gurubilliprudhvi@gmail.com>
2025-09-02 12:41:01 +0530

    Add files via upload

---
+++ Switchcase.js
@@ -0,0 +1,20 @@
+
+let marks=100;
+let grade="A";
+switch(marks=100)
+{
+    case"A":marks>=90;
+    break;
+
+    case "B":marks>=75;
+    break;
+
+    case "C":marks>=50;
+    break;
+
+    case "Fail":marks>=0;
+    break;
+    
+}
+console.log("marks",marks);
+console.log("Grade",grade);
