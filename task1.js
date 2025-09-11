e6bd584e4d12e65c21c78274baeb32809978a22f
Prudhvigurubilli04 <gurubilliprudhvi@gmail.com>
2025-09-11 10:24:17 +0530

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,33 +1,16 @@
-let passingMarks = 50;
-
-function processStudents(students) {
-    let updatedStudents = students.map(student => ({
-        name: student.name.toUpperCase(),
-        marks: student.marks
-    }));
-
-    let passedStudents = updatedStudents.filter(student => student.marks >= passingMarks);
-
-    passedStudents.forEach(student => {
-        let grade;
-        if (student.marks >= 90) grade = 'A';
-        else if (student.marks >= 75) grade = 'B';
-        else if (student.marks >= 65) grade = 'C';
-        else if (student.marks >= 50) grade = 'D';
-        else grade = 'F';
-
-        let shortName = student.name.slice(0, 3);
-        let hasA = student.name.includes('A') ? 'Yes' : 'No';
-
-        console.log(`Name: ${shortName}, Marks: ${student.marks}, Grade: ${grade}, Has 'A': ${hasA}`);
-    });
-}
-
-let students = [
-    { name: "Teja", marks: 92 },
-    { name: "praveen", marks: 20 },
-    { name: "kishore", marks: 75 },
-    { name: "divkar", marks: 50 }
+let student=[
+    { name: 'Prudhvi', age:22, marks:90 },
+    { name: 'tejas', age:23, marks:80 },
+    { name:'sivaji', age:22, marks:70 },
 ];
+let sum=0;
+let average=0;
+function avg(student){
+    for(let i=0;i<student.length;i++){
+        sum=sum+student[i].marks;
+        average=sum/student.length
+    }
+    console.log(average);
+}
+avg(student)
 
-processStudents(students);
